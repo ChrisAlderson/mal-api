cb57014b476d210673c69a97321a601793a14db2
Chris Alderson <chrisalderson@protonmail.com>
2017-07-18 08:36:32 +0200

    move to standard

--- mal-api.js
+++ mal-api.js
@@ -1,19 +1,16 @@
-'use strict';
+'use strict'
 
-const Account = require('./lib/Account');
-const Anime = require('./lib/Anime');
-const Helper = require('./lib/Helper');
-const Item = require('./lib/Item');
-const Manga = require('./lib/Manga');
+const Account = require('./lib/Account')
+const Anime = require('./lib/Anime')
+const Helper = require('./lib/Helper')
+const Manga = require('./lib/Manga')
 
 module.exports = class MalAPI {
+  constructor (user, pass, debug = false) {
+    const helper = new Helper(user, pass, debug)
 
-  constructor(user, pass, debug = false) {
-    const helper = new Helper(user, pass, debug);
-
-    this.account = new Account(helper);
-    this.anime = new Anime(helper);
-    this.manga = new Manga(helper);
+    this.account = new Account(helper)
+    this.anime = new Anime(helper)
+    this.manga = new Manga(helper)
   }
-
 }
