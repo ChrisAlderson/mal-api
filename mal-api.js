13ab34c9ded0b4919125c4bfd03d1d1ab6bfb97e
Chris Alderson <chrisalderson@protonmail.com>
2017-07-18 09:56:24 +0200

    update deps

--- mal-api.js
+++ mal-api.js
@@ -6,11 +6,13 @@ const Helper = require('./lib/Helper')
 const Manga = require('./lib/Manga')
 
 module.exports = class MalAPI {
-  constructor (user, pass, debug = false) {
+
+  constructor(user, pass, debug = false) {
     const helper = new Helper(user, pass, debug)
 
     this.account = new Account(helper)
     this.anime = new Anime(helper)
     this.manga = new Manga(helper)
   }
+
 }
