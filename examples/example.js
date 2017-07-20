266a3c3128657d65e4c5f471007722591d35f163
Chris Alderson <chrisalderson@protonmail.com>
2017-07-20 22:26:56 +0000

    refine examples

--- examples/example.js
+++ examples/example.js
@@ -1,46 +1,51 @@
 'use strict'
 
+// Import the necessary modules.
 const MalAPI = require('../mal-api')
 
+// Create a new instance of the module.
 const mal = new MalAPI(
   process.env.MAL_USER,
   process.env.MAL_PASS
 )
 
-mal.account.verifyCredentials()
-  .then(res => console.log(res))
-  .catch(err => console.error(err))
-
-mal.anime.searchAnime('naruto')
-  .then(res => console.log(res))
-  .catch(err => console.error(err))
-
-mal.anime.addAnime(1535, {
-  score: 10
+// Verify the credentials of a user and then start the CRUD operations for
+// animes.
+mal.account.verifyCredentials().then(res => {
+  console.log(res)
+  return mal.anime.searchAnime('naruto')
 }).then(res => {
   console.log(res)
-  return mal.anime.updateAnime(1535, {
+  return mal.anime.addAnime(21, {
     score: 10
   })
 }).then(res => {
   console.log(res)
-  return mal.anime.deleteAnime(1535)
-}).then(res => console.log(res))
-  .catch(err => console.error(err))
-
-mal.manga.searchManga('naruto')
-  .then(res => console.log(res))
-  .catch(err => console.error(err))
-
-mal.manga.addManga(1535, {
-  score: 10
-}).then(res => {
-  console.log(res)
-  return mal.manga.updateManga(1535, {
+  return mal.anime.updateAnime(21, {
     score: 10
   })
 }).then(res => {
   console.log(res)
-  return mal.manga.deleteManga(1535)
+  return mal.anime.deleteAnime(21)
 }).then(res => console.log(res))
   .catch(err => console.error(err))
+
+// The same for mangas.
+// mal.account.verifyCredentials().then(res => {
+//   console.log(res)
+//   return mal.manga.searchManga('naruto')
+// }).then(res => {
+//   console.log(res)
+//   return mal.manga.addManga(1535, {
+//     score: 10
+//   })
+// }).then(res => {
+//   console.log(res)
+//   return mal.manga.updateManga(1535, {
+//     score: 10
+//   })
+// }).then(res => {
+//   console.log(res)
+//   return mal.manga.deleteManga(1535)
+// }).then(res => console.log(res))
+//   .catch(err => console.error(err))
