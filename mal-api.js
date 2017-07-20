978387101baba3a0be30ed663e570bd6f0faed6c
Chris Alderson <chrisalderson@protonmail.com>
2017-07-20 23:59:35 +0000

    added esdoc comments

--- mal-api.js
+++ mal-api.js
@@ -1,17 +1,43 @@
 'use strict'
 
+// Import the necessary modules.
 const Account = require('./lib/Account')
 const Anime = require('./lib/Anime')
 const Helper = require('./lib/Helper')
 const Manga = require('./lib/Manga')
 
-module.exports = class MalAPI {
+/**
+ * A NodeJS wrapper for MyAnimeList API.
+ * @type {MalApi}
+ */
+// module.exports = class MalApi {
+export default class MalApi {
 
+  /**
+   * Create a new instance of the module.
+   * @param {!string} user - The username of the user.
+   * @param {!string} pass - The password of the user.
+   * @param {?boolean} [debug=false] - Show extra output.
+   */
   constructor(user, pass, debug = false) {
     const helper = new Helper(user, pass, debug)
 
+    /**
+     * The account section of the module.
+     * @type {Account}
+     */
     this.account = new Account(helper)
+
+    /**
+     * The anime section of the module.
+     * @type {Anime}
+     */
     this.anime = new Anime(helper)
+
+    /**
+     * The manga section of the module.
+     * @type {Manga}
+     */
     this.manga = new Manga(helper)
   }
 
